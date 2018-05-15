ea61516d9d9da6ea6c6999157c086db749347ae9
arte241_nagamani <arte241_nagamani@AT-R9.ariatech.it>
2018-05-15 14:33:07 +0530

    maven project

---
+++ adsub/src/main/java/primusbank/adsub/ad.java
@@ -0,0 +1,20 @@
+package primusbank.adsub;
+
+public class ad {
+	
+	 public void plus() {
+		 
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int x=10;
+	int y=20;
+	int z;
+	z=x+y;
+	System.out.println("sum of integers="+z);
+			
+
+	}
+
+}
+
---
+++ adsub/src/test/java/primusbank/adsub/as.java
@@ -0,0 +1,16 @@
+package primusbank.adsub;
+
+import org.testng.annotations.Test;
+
+public class as extends ad{
+@Test(priority=1)
+	public void vplus() {
+	plus();
+}
+
+}
+	
+	
+	
+
+
