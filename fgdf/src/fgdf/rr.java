ddffd2f4ea2142f7002adff136e7d45d6ab08775
arte241_nagamani <arte241_nagamani@AT-R9.ariatech.it>
2018-05-21 11:03:24 +0530

    bnm

---
+++ fgdf/src/fgdf/rr.java
@@ -0,0 +1,19 @@
+package fgdf;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class rr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver","C:\\Users\\arte241_nagamani\\Desktop\\chromedriver_win32 (1)\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://primusbank.qedgetech.com/");
+	}
+		
+		
+	}
+
+
